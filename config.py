f2a50875a8d6490fcf54e1aa8d03689a6bf6d2f5
seonghyeon <locing45@gmail.com>
2022-02-23 16:56:58 +0900

    flask api server AI-converter

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
-
 import boto3
 import tensorflow_hub as hub
-import flask
+import os
+import json
+
+with open('config.json') as f:
+    aws = json.loads(f.read())
+
+AWS_ACCESS_KEY_ID = aws['AWS']['ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = aws['AWS']['SECRET_ACCESS_KEY']
+
 
 class NstappConfig:
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'nstapp'
     hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-    s3 = boto3.client('s3', aws_access_key_id='AKIAWFK2LWOXX23LBLE4', aws_secret_access_key='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', region_name='ap-northeast-2')
+    s3 = boto3.client('s3', aws_access_key_id= AWS_ACCESS_KEY_ID, aws_secret_access_key= AWS_SECRET_ACCESS_KEY, region_name='ap-northeast-2')
 
